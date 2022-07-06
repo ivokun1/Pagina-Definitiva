e9b85d7d8a2192ca6c80e14a1d3f513facfeac7e
info-4 <info-4@INFO4-PC1>
2022-07-06 10:40:57 -0300

    Sacaffolding

--- Pagina Definitiva/Program.cs
+++ Pagina Definitiva/Program.cs
@@ -1,8 +1,18 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Pagina_Definitiva.Data;
 //using Pagina_Definitiva.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("Pagina_DefinitivaContextConnection") ?? throw new InvalidOperationException("Connection string 'Pagina_DefinitivaContextConnection' not found.");
+
+builder.Services.AddDbContext<Pagina_DefinitivaContext>(options =>
+    options.UseSqlite(connectionString));;
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<Pagina_DefinitivaContext>();;
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -26,5 +36,6 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+app.UseAuthentication();;
 
 app.Run();
